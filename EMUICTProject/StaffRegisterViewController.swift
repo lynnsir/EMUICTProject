09daa38d4923b27192d24b46c61462d68e812fb1
Lynn <sirinda@2405-9800-bc10.44.pool1.sila1-bcr02.fibre.ais>
2018-01-30 13:35:08 +0700

    1

---
+++ EMUICTProject/StaffRegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  StaffRegisterViewController.swift
+//  EMUICTProject
+//
+//  Created by Lynn on 1/30/2561 BE.
+//  Copyright © 2561 Sirinda. All rights reserved.
+//
+
+import UIKit
+
+class StaffRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
