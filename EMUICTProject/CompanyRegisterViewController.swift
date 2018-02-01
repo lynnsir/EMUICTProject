840e6946942918c4439aa527635798a41c54ac44
Lynn <sirinda@2405-9800-bc10.44.pool1.sila1-bcr02.fibre.ais>
2018-02-01 16:06:58 +0700

    ยังไม่เสร็จ

--- EMUICTProject/CompanyRegisterViewController.swift
+++ EMUICTProject/CompanyRegisterViewController.swift
@@ -7,15 +7,114 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class CompanyRegisterViewController: UIViewController {
+class CompanyRegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var companyName: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var conPassword: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var contactNumber: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var insertImageBtn: UIButton!
+    @IBOutlet weak var ContinueBtn: UIButton!
+    
+    let picker = UIImagePickerController()
+    var userStorage: StorageReference!
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        picker.delegate = self
+        
+        let storage = Storage.storage().reference(forURL:"gs://emuictproject-8baae.appspot.com")
+        
+        ref = Database.database().reference()
+        userStorage = storage.child("users")
+        
+ 
+        
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func insertImagePressed(_ sender: Any) {
+        
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.imageView.image = image
+            ContinueBtn.isHidden = false
+        }
+        self.dismiss(animated: true, completion:nil)
+    }
+    
+    /* @IBOutlet weak var companyName: UITextField!
+     @IBOutlet weak var username: UITextField!
+     @IBOutlet weak var password: UITextField!
+     @IBOutlet weak var conPassword: UITextField!
+     @IBOutlet weak var email: UITextField!
+     @IBOutlet weak var contactNumber: UITextField!
+     @IBOutlet weak var imageView: UIImageView!
+     @IBOutlet weak var insertImageBtn: UIButton!
+     @IBOutlet weak var ContinueBtn: UIButton!*/
+    
+    
+    
+    @IBAction func ContinuePressed(_ sender: Any) {
+        if companyName.text == nil || username.text == nil || password.text == nil ||
+            conPassword.text == nil || email.text == nil || contactNumber.text == nil
+        {
+            displyAlertMessage(userMessage: "Information is missing")
+        }
+            
+        
+        if password.text == conPassword.text
+        {
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: { (user, error) in
+                
+                if let error = error
+                {
+                    print(error.localizedDescription)
+                }
+            
+                else
+                {
+                    
+                }
+                
+            })
+        }
+            
+            else{
+            displyAlertMessage(userMessage:"Password doesn't match!")
+        
+        }
+        
+        
+    }
+    
+    
+    
+    func displyAlertMessage(userMessage:String){
+        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.alert);
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        
+        myAlert.addAction(okAction);
+        
+        self.present(myAlert,animated: true, completion:nil)
+    }
+    
+    
+    
+   
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
