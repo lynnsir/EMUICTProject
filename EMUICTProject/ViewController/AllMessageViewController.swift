2d1b1a94088d368722b23a03748eb729f3912126
Teeraphon <teeraphon.iss@gmail.com>
2018-04-02 08:49:47 +0700

    chat user model

---
+++ EMUICTProject/ViewController/AllMessageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AllMessageViewController.swift
+//  EMUICTProject
+//
+//  Created by Teeraphon Issaranuluk on 2/4/2561 BE.
+//  Copyright © 2561 Sirinda. All rights reserved.
+//
+
+import UIKit
+
+class AllMessageViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
