601d517917cbbb299740752f8ecaf541216593bc
Lynn <sirinda@sirindas-macbook-pro.local>
2018-04-13 16:39:32 +0700

    payment

--- EMUICTProject/ViewController/InvoiceViewController.swift
+++ EMUICTProject/ViewController/InvoiceViewController.swift
@@ -15,6 +15,7 @@ class InvoiceViewController: UIViewController {
    
     var name:String!
     var type:String!
+    var db:String!
   
     @IBOutlet weak var fullname: UILabel!
     @IBOutlet weak var memberType: UILabel!
@@ -29,15 +30,19 @@ class InvoiceViewController: UIViewController {
         
         if memberType.text == "Student"{
             price.text = "150.00"
+            self.db = "Student user"
         }
         else if memberType.text == "Staff"{
             price.text = "250.00"
+            self.db = "Staff user"
         }
         else if memberType.text == "Alumni"{
             price.text = "200.00"
+            self.db = "Alumni user"
         }
         else if memberType.text == "Company"{
             price.text = "300.00"
+            self.db = "Company user"
         }
         
     }
@@ -57,7 +62,8 @@ class InvoiceViewController: UIViewController {
         let user = Auth.auth().currentUser
         let user2 = Auth.auth().currentUser?.uid
         let user3 = user2
-        
+        let storage = Storage.storage().reference(forURL:"gs://emuictproject-8baae.appspot.com")
+        userStorage = storage.child(self.db)
         let imageRef = userStorage.child(user2!+".jpg")
         print(user2!+".jpg")
         imageRef.delete(completion: { error in
